ed6ab0b6d83d0714d44dfe98115b24ae10ecaa67
leonardobarbosas <leonardobarbosasantos@gmail.com>
2026-04-29 19:36:50 -0300

    subindo com axios

--- app/(tabs)/rewards.tsx
+++ app/(tabs)/rewards.tsx
@@ -1,8 +1,21 @@
 import { Reward, rewards } from "@/data/rewards";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { FlatList, Text, View } from "react-native";
 
 export default function TabTwoScreen() {
+  useEffect(() => {
+    async function loadData() {
+      console.log("pre.load");
+      const response = await axios.get(
+        "https://fidelidade-api.onrender.com/v1/programas",
+      );
+      console.log("pos.load");
+    }
+
+    loadData();
+  }, []);
+
   const [isRefreshing, setIsRefreshing] = useState(false);
   const item = ({ item }: { item: Reward }) => (
     <View className="bg-[#FFE3D1] p-6">
